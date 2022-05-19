e62fd668b8c033274b354b785b6e33ce57b4a5c8
Justin Pealing <jpealing@fiscaltec.com>
2022-05-19 17:33:20 +0100

    Initial `GrafanaRuleGroup` implementation

---
+++ pkg/grafana/grafanarulegroup-handler.go
@@ -0,0 +1,114 @@
+package grafana
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/grafana/grizzly/pkg/grizzly"
+	"github.com/grafana/tanka/pkg/kubernetes/manifest"
+)
+
+// GrafanaRuleGroupHandler is a Grizzly Handler for Grafana alert rules
+type GrafanaRuleGroupHandler struct {
+	Provider Provider
+}
+
+// NewGrafanaRuleGroupHandler returns configuration defining a new Grafana Rule Group Handler
+func NewGrafanaRuleGroupHandler(provider Provider) *GrafanaRuleGroupHandler {
+	return &GrafanaRuleGroupHandler{
+		Provider: provider,
+	}
+}
+
+// Kind returns the kind for this handler
+func (h *GrafanaRuleGroupHandler) Kind() string {
+	return "GrafanaRuleGroup"
+}
+
+// Validate returns the uid of resource
+func (h *GrafanaRuleGroupHandler) Validate(resource grizzly.Resource) error {
+	uid, exist := resource.GetSpecString("uid")
+	if exist {
+		if uid != resource.Name() {
+			return fmt.Errorf("uid '%s' and name '%s', don't match", uid, resource.Name())
+		}
+	}
+
+	return nil
+}
+
+// APIVersion returns group and version of the provider of this resource
+func (h *GrafanaRuleGroupHandler) APIVersion() string {
+	return h.Provider.APIVersion()
+}
+
+// GetExtension returns the file name extension for a datasource
+func (h *GrafanaRuleGroupHandler) GetExtension() string {
+	return "yaml"
+}
+
+const (
+	grafanaruleGlob    = "grafanarulegroups/grafanarulegroup-*"
+	grafanarulePattern = "grafanarulegroups/grafanarulegroup-%s.%s"
+)
+
+// FindResourceFiles identifies files within a directory that this handler can process
+func (h *GrafanaRuleGroupHandler) FindResourceFiles(dir string) ([]string, error) {
+	path := filepath.Join(dir, grafanaruleGlob)
+	return filepath.Glob(path)
+}
+
+// ResourceFilePath returns the location on disk where a resource should be updated
+func (h *GrafanaRuleGroupHandler) ResourceFilePath(resource grizzly.Resource, filetype string) string {
+	return fmt.Sprintf(grafanarulePattern, resource.UID(), filetype)
+}
+
+// Parse parses a manifest object into a struct for this resource type
+func (h *GrafanaRuleGroupHandler) Parse(m manifest.Manifest) (grizzly.Resources, error) {
+	resource := grizzly.Resource(m)
+	return grizzly.Resources{resource}, nil
+}
+
+// Unprepare removes unnecessary elements from a remote resource ready for presentation/comparison
+func (h *GrafanaRuleGroupHandler) Unprepare(resource grizzly.Resource) *grizzly.Resource {
+	return &resource
+}
+
+// Prepare gets a resource ready for dispatch to the remote endpoint
+func (h *GrafanaRuleGroupHandler) Prepare(existing, resource grizzly.Resource) *grizzly.Resource {
+	return &resource
+}
+
+// GetUID returns the UID for a resource
+func (h *GrafanaRuleGroupHandler) GetUID(resource grizzly.Resource) (string, error) {
+	if !resource.HasMetadata("folder") {
+		return "", fmt.Errorf("%s %s requires a folder metadata entry", h.Kind(), resource.Name())
+	}
+	return fmt.Sprintf("%s.%s", resource.GetMetadata("folder"), resource.Name()), nil
+}
+
+// GetByUID retrieves JSON for a resource from an endpoint, by UID (name)
+func (h *GrafanaRuleGroupHandler) GetByUID(UID string) (*grizzly.Resource, error) {
+	return getRemoteGrafanaRuleGroup(UID)
+}
+
+// GetRemote retrieves a rule group as a Resource
+func (h *GrafanaRuleGroupHandler) GetRemote(resource grizzly.Resource) (*grizzly.Resource, error) {
+	uid := fmt.Sprintf("%s.%s", resource.GetMetadata("folder"), resource.Name())
+	return getRemoteGrafanaRuleGroup(uid)
+}
+
+// ListRemote retrieves as list of UIDs (names) of all remote rule groups
+func (h *GrafanaRuleGroupHandler) ListRemote() ([]string, error) {
+	return getRemoteGrafanaRuleGroupList()
+}
+
+// Add pushes a rule group to Grafana via the API
+func (h *GrafanaRuleGroupHandler) Add(resource grizzly.Resource) error {
+	return postGrafanaRuleGroup(resource)
+}
+
+// Update pushes a rule group to Grafana via the API
+func (h *GrafanaRuleGroupHandler) Update(existing, resource grizzly.Resource) error {
+	return postGrafanaRuleGroup(resource)
+}
---
+++ pkg/grafana/grafanarulegroups.go
@@ -0,0 +1,175 @@
+package grafana
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/grafana/grizzly/pkg/grizzly"
+)
+
+// getRemoteGrafanaRuleGroup retrieves a rule group from Grafana
+func getRemoteGrafanaRuleGroup(uid string) (*grizzly.Resource, error) {
+	parts := strings.SplitN(uid, ".", 2)
+	folder := parts[0]
+	name := parts[1]
+
+	client := new(http.Client)
+	h := GrafanaRuleGroupHandler{}
+	grafanaURL, err := getGrafanaURL(fmt.Sprintf("api/ruler/grafana/api/v1/rules/%s/%s", folder, name))
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", grafanaURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if grafanaToken, ok := getGrafanaToken(); ok {
+		req.Header.Set("Authorization", "Bearer "+grafanaToken)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusNotFound:
+		return nil, fmt.Errorf("NOT FOUND")
+	default:
+		if resp.StatusCode >= 400 {
+			return nil, errors.New(resp.Status)
+		}
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var g RuleGroupConfig
+	if err := json.Unmarshal([]byte(string(body)), &g); err != nil {
+		return nil, grizzly.APIErr{Err: err, Body: body}
+	}
+
+	resource := grizzly.NewResource(h.APIVersion(), h.Kind(), name, g)
+	resource.SetMetadata("folder", folder)
+	return &resource, nil
+}
+
+func getRemoteGrafanaRuleGroupList() ([]string, error) {
+	client := new(http.Client)
+	UIDs := []string{}
+	grafanaURL, err := getGrafanaURL("api/ruler/grafana/api/v1/rules")
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", grafanaURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if grafanaToken, ok := getGrafanaToken(); ok {
+		req.Header.Set("Authorization", "Bearer "+grafanaToken)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	switch {
+	case resp.StatusCode == http.StatusNotFound:
+		return nil, fmt.Errorf("couldn't fetch rule list from remote: %w", grizzly.ErrNotFound)
+	case resp.StatusCode >= 400:
+		return nil, errors.New(resp.Status)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	groupings := map[string][]GettableRuleGroupConfig{}
+	if err := json.Unmarshal([]byte(string(body)), &groupings); err != nil {
+		return nil, grizzly.APIErr{Err: err, Body: body}
+	}
+
+	for folder, grouping := range groupings {
+		for _, group := range grouping {
+			uid := fmt.Sprintf("%s.%s", folder, group.Name())
+			UIDs = append(UIDs, uid)
+		}
+	}
+
+	return UIDs, nil
+}
+
+func postGrafanaRuleGroup(resource grizzly.Resource) error {
+	folder := resource.GetMetadata("folder")
+
+	client := new(http.Client)
+	grafanaURL, err := getGrafanaURL(fmt.Sprintf("api/ruler/grafana/api/v1/rules/%s", folder))
+	if err != nil {
+		return err
+	}
+
+	bs, err := json.Marshal(resource["spec"])
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", grafanaURL, bytes.NewBuffer(bs))
+	if err != nil {
+		return err
+	}
+
+	if grafanaToken, ok := getGrafanaToken(); ok {
+		req.Header.Set("Authorization", "Bearer "+grafanaToken)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	switch resp.StatusCode {
+	case http.StatusAccepted:
+		return nil
+	default:
+		return NewErrNon200Response("rules", resource.Name(), resp)
+	}
+}
+
+type RuleGroupConfig map[string]interface{}
+
+type GettableRuleGroupConfig map[string]interface{}
+
+func (d *GettableRuleGroupConfig) Name() string {
+	name, ok := (*d)["name"]
+	if !ok {
+		return ""
+	}
+	return name.(string)
+}
+
+type GettableExtendedRuleNode struct {
+	GrafanaAlert GrafanaRule `json:"grafana_alert"`
+}
+
+type GrafanaRule map[string]interface{}
+
+func (d *GrafanaRule) UID() string {
+	uid, ok := (*d)["uid"]
+	if !ok {
+		return ""
+	}
+	return uid.(string)
+}
